cfbff54e74cec9287c98bc297acd92bf37428778
Snipeur060 <61784407+Snipeur060@users.noreply.github.com>
2023-06-02 22:59:44 +0200

    Update main.py

--- pythonv/main.py
+++ pythonv/main.py
@@ -1,18 +1,32 @@
 from func.aianalyze import predict
 from rich.console import Console
 from func.webinteract import dlimageandreturndata
+from func.langage import checklangage
 console = Console()
+langage = checklangage()
+
+def menu() -> None:
+    """
+    Affiche le menu
+    :return: None
+    """
+    console.print("Pour quitter le programme, entrez 'stop' ou 'exit'", style="bold blue")
+    console.print("Pour changer de modèle, entrez 'low' ou 'high'", style="bold blue")
+    console.print("Pour changer d'image, entrez 'non', 'nop' ou 'no'", style="bold blue")
+    console.print("Pour continuer, entrez le chemin de l'image", style="bold blue")
+
+
 while True:
-    img_path = console.input("Chemin de l'image: ")
+    img_path = console.input(langage["askimgpath"])
     predict_model = 0
     while predict_model != "low" and predict_model != "high":
-        predict_model = console.input("Modèle à utiliser (low/high): ")
+        predict_model = console.input(langage["modeltouse"])
         #si while toujours pas fini
         if predict_model != "low" and predict_model != "high":
-            console.print("Veuillez entrer une valeur correcte", style="bold red")
+            console.print(langage["wrongvalue"], style="bold red")
     #ajout du support d'une url
     if img_path in ["stop","exit","non","nop","no"]:
-        console.print("Arrêt du programme", style="bold red")
+        console.print(langage["programstop"], style="bold red")
         exit()
     if img_path.startswith("http"):
         img_data = dlimageandreturndata(img_path)
@@ -20,7 +34,7 @@ while True:
         try:
             img_data = open(img_path, 'rb').read()
         except FileNotFoundError:
-            console.print("Le chemin de l'image est invalide", style="bold red")
+            console.print(langage["invalidpath"], style="bold red")
             continue
     analyze = predict(img_data, predict_model)
     if "Erreur" in analyze:
