1370319e3cb064bcbff5ba8eee8532e7ef3c7f67
Snipeur060 <61784407+Snipeur060@users.noreply.github.com>
2023-06-02 22:46:45 +0200

    Error control

--- pythonv/main.py
+++ pythonv/main.py
@@ -12,14 +12,18 @@ while True:
             console.print("Veuillez entrer une valeur correcte", style="bold red")
     #ajout du support d'une url
     if img_path in ["stop","exit","non","nop","no"]:
-      exit()
+        console.print("Arrêt du programme", style="bold red")
+        exit()
     if img_path.startswith("http"):
         img_data = dlimageandreturndata(img_path)
     else:
-        img_data = open(img_path, 'rb').read()
+        try:
+            img_data = open(img_path, 'rb').read()
+        except FileNotFoundError:
+            console.print("Le chemin de l'image est invalide", style="bold red")
+            continue
     analyze = predict(img_data, predict_model)
     if "Erreur" in analyze:
         console.print(analyze, style="bold red")
     else:
         console.print(analyze, style="bold green")
-
