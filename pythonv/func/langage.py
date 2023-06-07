7263da358da1b28561d936ddd6f6cf8e53498793
Snipeur060 <61784407+Snipeur060@users.noreply.github.com>
2023-06-07 20:24:26 +0200

    Changement langage

--- pythonv/func/langage.py
+++ pythonv/func/langage.py
@@ -1,3 +1,6 @@
+from rich.console import Console
+console = Console()
+
 fr = {
     "askimgpath": "Veuillez entrer le chemin de l'image: ",
     "modeltouse": "Modèle à utiliser (low/high): ",
@@ -5,6 +8,17 @@ fr = {
     "programstop": "Arrêt du programme",
     "invalidpath": "Le chemin de l'image est invalide",
     "urlnotvalid": "L'url n'est pas valide",
+    "menu": """ 
+    1) Analyser une image
+    2) Changer la langue
+    3) Vérifier les mises à jour
+    4) Voir les crédits
+    5) Quitter
+    6) Vider la console
+    """,
+    "chooseoption": "Veuillez choisir une option: ",
+    "asklangage": "Veuillez choisir une langue (fr/en): ",
+    "resetmessage": "La langue a été réinitialisée. Il faut redémarrer le programme pour que les changements soient pris en compte."
 }
 en = {
     "askimgpath": "Please enter the path of the image: ",
@@ -13,8 +27,19 @@ en = {
     "programstop": "Program stop",
     "invalidpath": "The path of the image is invalid",
     "urlnotvalid": "The url is not valid",
+    "menu": """
+    1) Analyze an image
+    2) Change language
+    3) Check for updates
+    4) See credits
+    5) Quit
+    6) Clear console
+    """,
+    "chooseoption": "Please choose an option: ",
+    "asklangage": "Please choose a language (fr/en): ",
+    "resetmessage": "The language has been reset. You have to restart the program for the changes to take effect."
 }
-
+console = Console()
 def checklangage():
     """
     On ouvre le fichier langage.txt et on récupère la langue
@@ -26,7 +51,7 @@ def checklangage():
         return returnlangage(langage)
     else:
         while True:
-            langage = input("Veuillez entrer une langue valide (fr/en):")
+            langage = input(en["asklangage"])
             if langage == "fr" or langage == "en":
                 break
         with open("func/langage.txt", "w") as f:
@@ -58,3 +83,13 @@ def customcheck():
         return returnlangage(langage)
     else:
         return en
+
+def resetlangage():
+    """
+    Reset la langue
+    :return: None
+    """
+    with open("func/langage.txt", "w") as f:
+        f.write("None")
+    checklangage()
+    console.print(en["resetmessage"], style="bold green")
